2af9f58e5018ae2cffa7b49a27fb92f7bd0bc175
sprogrammer12 <89727991+sprogrammer12@users.noreply.github.com>
2025-01-29 15:58:52 +1100

    added clientside js to test button functionality

--- server.js
+++ server.js
@@ -28,6 +28,11 @@ const rappers = {
     } 
 }
 
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     next();
+// });
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
