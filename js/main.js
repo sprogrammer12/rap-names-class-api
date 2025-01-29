073ea979d605e6a6bd52088cc12a8d8159a14f81
sprogrammer12 <89727991+sprogrammer12@users.noreply.github.com>
2025-01-29 17:02:03 +1100

    fixing input value

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 document.querySelector("button").addEventListener("click", response)
 
-function response(){
-    const rapperName = document.getElementById("rappersName").value
-    console.log(rapperName)
-    alert('response')
-}
-
-
-//  async function response(){
-//     const rapperName = document.querySelector("input").value
-
-//     try{
-//         console.log(rapperName)
-//         const res = await fetch(`https://rap-names-class-api-render.onrender.com/api/${rapperName}`)
-//         const data = await res.json()
-//         console.log(data)
-//         document.querySelector("h2").innerText = data.birthName
-//     }
-//     catch(err){
-//         console.log(err)
-//     }
+// function response(){
+//     const rapperName = document.getElementById("rappersName").value
+//     console.log(rapperName)
+//     alert('response')
 // }
+
+
+ async function response(){
+    const rapperName = document.getElementById("rappersName").value
+
+    try{
+        console.log(rapperName)
+        const res = await fetch(`https://rap-names-class-api-render.onrender.com/api/${rapperName}`)
+        const data = await res.json()
+        console.log(data)
+        document.querySelector("h2").innerText = data.birthName
+    }
+    catch(err){
+        console.log(err)
+    }
+}
