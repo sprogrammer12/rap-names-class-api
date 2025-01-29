22dd6490bf237ca8685f28f02d1e44fd64116632
sprogrammer12 <89727991+sprogrammer12@users.noreply.github.com>
2025-01-29 16:49:18 +1100

    debugging

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
 document.querySelector("button").addEventListener("click", response)
 
-// function response(){
-//     console.log('response')
-//     alert('response')
-// }
-
-
- async function response(){
+function response(){
     const rapperName = document.querySelector("input").value
-
-    try{
-        const res = await fetch(`https://rap-names-class-api-render.onrender.com/api/${rapperName}`)
-        const data = await res.json()
-        console.log(data)
-        document.querySelector("h2").innerText = data.birthName
-    }
-    catch(err){
-        console.log(err)
-    }
+    console.log(rapperName)
+    alert('response')
 }
+
+
+//  async function response(){
+//     const rapperName = document.querySelector("input").value
+
+//     try{
+//         console.log(rapperName)
+//         const res = await fetch(`https://rap-names-class-api-render.onrender.com/api/${rapperName}`)
+//         const data = await res.json()
+//         console.log(data)
+//         document.querySelector("h2").innerText = data.birthName
+//     }
+//     catch(err){
+//         console.log(err)
+//     }
+// }
