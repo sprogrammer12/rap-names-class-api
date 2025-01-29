e90dcd06538fe70e778375e562ee4161dca0cd78
sprogrammer12 <89727991+sprogrammer12@users.noreply.github.com>
2025-01-29 16:11:00 +1100

    test if changes reflect on server

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 document.querySelector("button").addEventListener("click", response)
 
-function response(){
-    console.log('response')
-    alert('response')
-}
-
-
-//  async function response(){
-//     const rapperName = document.querySelector("input").value
-
-//     try{
-//         const res = await fetch(`https://rap-names-class-api-render.onrender.com/api/${rapperName}`)
-//         const data = await res.json()
-//         console.log(data)
-//         document.querySelector("h2").innerText += " " + data.birthName
-//     }
-//     catch(err){
-//         console.log(err)
-//     }
+// function response(){
+//     console.log('response')
+//     alert('response')
 // }
+
+
+ async function response(){
+    const rapperName = document.querySelector("input").value
+
+    try{
+        const res = await fetch(`https://rap-names-class-api-render.onrender.com/api/${rapperName}`)
+        const data = await res.json()
+        console.log(data)
+        document.querySelector("h2").innerText = data.birthName
+    }
+    catch(err){
+        console.log(err)
+    }
+}
